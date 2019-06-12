225340d2e102c0c23c88156b83c11044d9ba510b
Tharinigovind <tharinigt97@gmail.com>
2019-06-12 12:06:47 +0530

    react login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const initialState={
+ firstName:'',
+ lastName:'',
+ email:'',
+ password:''  };
+function reducer(state = initialState, action) {
+  console.log('login page')
+  switch(action.type) {
+case 'firstName':
+      return {
+        firstName: state.firstName
+       };
+case 'lastName':
+           return {
+            lastName:state.lastName
+       };
+case 'email':
+      return {
+        email:state.email
+       };
+    default:
+    return state
+  }
+}
+const store=createStore(reducer);
+const AppFull = () => (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+);
+ReactDOM.render(<AppFull />, document.getElementById('root'));
